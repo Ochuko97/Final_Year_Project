49d1fe0d0fb56757db352805953e1248c92b34ac
Ochuko97 <ochuko97@hotmail.com>
2017-11-22 13:22:39 +0000

    With the help of Chris implemented Gachapoint demo

---
+++ Final Year Artefact/Assets/Scripts/PedometerConnect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedometerConnect : MonoBehaviour {
+
+    private PedometerPlugin pedometerPlugin;
+    private string demoName = "[PedometerDemo] ";
+    private UtilsPlugin utilsPlugin;
+
+    private int stepCount = 0;
+    public int StepCount { get { return stepCount; } }
+
+    // Use this for initialization
+    void Start () {
+        //get the instance of pedometer plugin
+        pedometerPlugin = PedometerPlugin.GetInstance();
+
+        //set to zero to hide debug toast messages
+        pedometerPlugin.SetDebug(0);
+
+        utilsPlugin = UtilsPlugin.GetInstance();
+        utilsPlugin.SetDebug(0);
+
+        //check if step detector is supported on the current android mobile device
+        bool hasStepDetector = utilsPlugin.HasStepDetector();
+        bool hasStepCounter = utilsPlugin.HasStepCounter();
+
+        //if (hasStepDetector) {
+        if (hasStepCounter) {
+            pedometerPlugin.OnStepCount += OnStepCount;
+
+            //initialze pedometer
+            pedometerPlugin.Init();
+        }
+    }
+
+    private void OnStepCount(int totalStepCount) {
+        stepCount++;
+    }
+}
--- Final Year Artefact/Assets/Scripts/Progress_bar.cs
+++ Final Year Artefact/Assets/Scripts/Progress_bar.cs
@@ -6,32 +6,30 @@ using UnityEngine.UI;
 public class Progress_bar : MonoBehaviour {
 	// setting the progress bar fill as a public variable that can be updated through the inspector 
 	public Image bar;  
-	// public number taht will indicate which percent of the progress bar would be filled
-	public float fill_no;  
-	public float steps; 
+	
+    public PedometerConnect PedometerConnection;
+    public int CurrentSteps;
 
-
+    public int TargetSteps = 100;
+    public float Progress;
 
 	// Use this for initialization
 	void Start () { 
-		//setting the variable to zero will cause the progress bar to be empty
-		fill_no = 0.0f; 
-		//steps 
 
 	}
 	
 	// Update is called once per frame
 	void Update () { 
 		// the x scale value of the progress bar will constantly checking for and updating the fill ratio of the progress bar.
-		//bar.rectTransform.localScale = new Vector3 (fill_no, 1f, 1f);  
+		// 
 		// this initilse a second routine that will begin when the IEnumerator Updatebar () is called.
 		//StartCoroutine(Updatebar());  
 		//if the x value of the bar is higher than 1, anchor the value to 1. This ensures the par does not over fill 
-		if (fill_no >= 1) {
-			fill_no = 1f;
-			//Debug.Log("yay"); 
-		}
-	} 
+
+        CurrentSteps = PedometerConnection.StepCount;
+        Progress = Mathf.Min((float)CurrentSteps / (float)TargetSteps, 1f);
+        bar.rectTransform.localScale = new Vector3(Progress, 1f, 1f);
+    } 
 	//IEnumerator Updatebar ()
 	//{
 		//Once the coroutine is called wait 2 seconds and increase the bar value.
